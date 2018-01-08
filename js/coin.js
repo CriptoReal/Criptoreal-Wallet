50783f938d5f0f52031611f09b633e53c7f987ff
Criptoreal Dev <criptorealdev@gmail.com>
2018-01-08 07:36:18 -0300

    URL changes

--- js/coin.js
+++ js/coin.js
@@ -18,7 +18,7 @@
 	coinjs.compressed = false;
 
 	/* other vars */
-	coinjs.developer = '1CWHWkTWaq1K5hevimJia3cyinQsrgXUvg'; // bitcoin
+	coinjs.developer = 'CRiPToRGkyakNUq7nTYTNYpGoEj4f5frzd'; // CriptoReal
 
 	/* bit(coinb.in) api vars */
 	coinjs.host = ('https:'==document.location.protocol?'https://':'http://')+'coinb.in/api/';
